6705f5f28dda1f9bceb59386a401ca783e0891c1
Eric Ansah <ericbarrystilesansah@gmail.com>
2023-10-18 03:40:56 -0700

    4-utils.c betty compliance

--- 4-utils.c
+++ 4-utils.c
@@ -1,22 +1,27 @@
 #include "shell.h"
-
+/**
+ */
 int run_from_file(char *filename, char **av, char **env)
 {
-    FILE *file = fopen(filename, "r");
-    char line[MAX_CMD_LEN];
-    int i = 0, ex = 0;
+	FILE *file = fopen(filename, "r");
+	char line[MAX_CMD_LEN];
+	int i = 0, ex = 0;
 
-    (void) env;
-    if (file == NULL)
-    {
-        fprintf(stderr, "Failed to open the file %s\n", filename);
-        return (1);
-    }
-    while (fgets(line, sizeof(line), file) != NULL)
-    {
-        run_semis(line, &i, av, &ex, env);
+	(void) env;
+
+	if (file == NULL)
+	{
+		fprintf(stderr, "Failed to open the file %s\n", filename);
+
+		return (1);
     }
 
-    fclose(file);
-    return (ex);
+	while (fgets(line, sizeof(line), file) != NULL)
+	{
+		run_semis(line, &i, av, &ex, env);
+	}
+
+	fclose(file);
+
+	return (ex);
 }
