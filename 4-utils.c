fa23923d5eea70ab55e9de4d5af43f105f21ca6a
Eric Ansah <ericbarrystilesansah@gmail.com>
2023-10-18 03:45:07 -0700

    4-utils.c

--- 4-utils.c
+++ 4-utils.c
@@ -1,6 +1,5 @@
 #include "shell.h"
-/**
- */
+
 int run_from_file(char *filename, char **av, char **env)
 {
 	FILE *file = fopen(filename, "r");
@@ -12,10 +11,8 @@ int run_from_file(char *filename, char **av, char **env)
 	if (file == NULL)
 	{
 		fprintf(stderr, "Failed to open the file %s\n", filename);
-
 		return (1);
-    }
-
+	}
 	while (fgets(line, sizeof(line), file) != NULL)
 	{
 		run_semis(line, &i, av, &ex, env);
